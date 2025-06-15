c1ade879b3c9f440e04ac768e3be5a98ab80c4dc
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2025-06-15 14:19:28 -0300

    creating MyOpenAI class

--- src/openaiclient.py
+++ src/openaiclient.py
@@ -7,10 +7,38 @@ import os
 import dotenv
 from openai import OpenAI
 
-dotenv.load_dotenv("./env")
+dotenv.load_dotenv()
 
 API_KEY = os.environ["OPENAI-API-KEY"]
 
-# connecting with openai client
-client = OpenAI(api_key=API_KEY)
+if not API_KEY:
+    raise ValueError("OpenAI API key not found")
 
+
+class MyOpenAI():
+    """
+        Connects with openai api and generate responses    
+    """
+
+    def __init__(self, api_key=API_KEY) -> None:
+        # connecting with openai client
+        self.api_key = api_key
+        self.client = OpenAI(api_key=self.api_key)
+
+    def get_ai_response(self, prompt="write a test message") -> str:
+        response = self.client.responses.create(
+            model="gpt-4o",
+            input=[
+                {"role": "user", "content": prompt}
+                ]
+        )
+
+        # return response.output_text
+        return "teste"
+    
+if __name__ == "__main__":
+    foo = MyOpenAI()
+
+    msg = foo.get_ai_response()
+
+    print(msg)
